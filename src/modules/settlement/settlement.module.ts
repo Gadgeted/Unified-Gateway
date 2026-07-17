aa204e5e632f653197f4f0806a91b4a0c94514ba
Collins Ngunyi <collinsmaina1921@gmail.com>
2026-07-17 12:16:27 +0300

    fix: module

--- src/modules/settlement/settlement.module.ts
+++ src/modules/settlement/settlement.module.ts
@@ -1,12 +1,19 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { SettlementService } from './settlement.service';
 import { SettlementController } from './settlement.controller';
-import { MpesaModule } from '../mpesa/mpesa.module'; // ✅ import MpesaModule
+import { MpesaModule } from '../mpesa/mpesa.module';
 import { HybridAuthGuard } from '../../common/guards/hybrid-auth.guard';
 
 @Module({
   imports: [
-    MpesaModule, // ✅ makes B2cService available
+    MpesaModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'super_secret_gateway_key_for_development_2026',
+      signOptions: { expiresIn: '7d' },
+    }),
   ],
   controllers: [SettlementController],
   providers: [
